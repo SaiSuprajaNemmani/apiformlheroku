0ec5f247fa5f1bfe0c95f6cb018d28f8875abef8
Sai Supraja Nemmani <reachsaisupraja@gmail.com>
2023-04-02 20:28:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -39,6 +39,7 @@ class model_input(BaseModel):
 async def read_file():
     async with open('diabetes_model.sav','rb') as file:
         diabetes_model = await file.read()
+        delay = 5
         await asyncio.sleep(delay)
         print(diabetes_model) 
 
